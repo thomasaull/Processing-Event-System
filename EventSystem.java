e413e3f48bf89b8de3b4de0395a5e0337aa3e0ea
Thomas Aull <thomas.aull@daimler.com>
2014-02-11 18:04:32 -0800

    implemented removeEventListener functionality

--- EventSystem.java
+++ EventSystem.java
@@ -55,9 +55,24 @@ public class EventSystem
 		{
 			eventListener = new EventListener(_type, _containingClass, method, _eventClass);
 			eventListeners.add(eventListener);
+			//System.out.println("eventListener added | " + eventListeners.size());
 		}
 		else
 			System.out.println("error adding event to the listenerList");
+	}
+	
+	
+	public static void removeEventListener(Class _eventClass, String _type, Object _containingClass, String _method)
+	{
+		for(int i = 0; i < eventListeners.size(); i++)
+		{
+			EventListener eventListener = eventListeners.get(i);
+			if(eventListener.type == _type && eventListener.callbackClass == _containingClass && eventListener.callback.getName() == _method && eventListener.eventClass == _eventClass)
+			{
+				eventListeners.remove(eventListener);
+				//System.out.println("eventListener removed | " + eventListeners.size());
+			}
+		}
 	}
 	
 	
@@ -68,15 +83,17 @@ public class EventSystem
 	
 	
 	public static void dispatchEvent(String _type, Event _event)
-	{		
-		for(EventListener eventListener : eventListeners)
+	{	
+		for(int i = 0; i < eventListeners.size(); i++)
 		{
+			EventListener eventListener = eventListeners.get(i);
+			
 			if(eventListener.type == _type)
 			{				
 				try
 				{
 					eventListener.callback.invoke(eventListener.callbackClass, _event);
-				} 
+				}
 				catch (IllegalArgumentException e)
 				{
 					// TODO Auto-generated catch block
